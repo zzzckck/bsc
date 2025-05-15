c789c95d3edb4750b361e7f7f552e2556623384d
zzzckck <152148891+zzzckck@users.noreply.github.com>
2025-05-15 16:22:45 +0800

    update 5

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -396,18 +396,21 @@ func (s *StateDB) SubRefund(gas uint64) {
 // Exist reports whether the given account address exists in the state.
 // Notably this also returns true for self-destructed accounts.
 func (s *StateDB) Exist(addr common.Address) bool {
+	defer debug.Handler.StartRegionAuto("StateDB.Exist")()
 	return s.getStateObject(addr) != nil
 }
 
 // Empty returns whether the state object is either non-existent
 // or empty according to the EIP161 specification (balance = nonce = code = 0)
 func (s *StateDB) Empty(addr common.Address) bool {
+	defer debug.Handler.StartRegionAuto("StateDB.Empty")()
 	so := s.getStateObject(addr)
 	return so == nil || so.empty()
 }
 
 // GetBalance retrieves the balance from the given address or 0 if object not found
 func (s *StateDB) GetBalance(addr common.Address) *uint256.Int {
+	defer debug.Handler.StartRegionAuto("StateDB.GetBalance")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.Balance()
@@ -417,6 +420,7 @@ func (s *StateDB) GetBalance(addr common.Address) *uint256.Int {
 
 // GetNonce retrieves the nonce from the given address or 0 if object not found
 func (s *StateDB) GetNonce(addr common.Address) uint64 {
+	defer debug.Handler.StartRegionAuto("StateDB.GetNonce")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.Nonce()
@@ -428,6 +432,7 @@ func (s *StateDB) GetNonce(addr common.Address) uint64 {
 // GetStorageRoot retrieves the storage root from the given address or empty
 // if object not found.
 func (s *StateDB) GetStorageRoot(addr common.Address) common.Hash {
+	defer debug.Handler.StartRegionAuto("StateDB.GetStorageRoot")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.Root()
@@ -441,6 +446,7 @@ func (s *StateDB) TxIndex() int {
 }
 
 func (s *StateDB) GetCode(addr common.Address) []byte {
+	defer debug.Handler.StartRegionAuto("StateDB.GetCode")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		if s.witness != nil {
@@ -452,6 +458,7 @@ func (s *StateDB) GetCode(addr common.Address) []byte {
 }
 
 func (s *StateDB) GetRoot(addr common.Address) common.Hash {
+	defer debug.Handler.StartRegionAuto("StateDB.GetRoot")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.data.Root
@@ -460,6 +467,7 @@ func (s *StateDB) GetRoot(addr common.Address) common.Hash {
 }
 
 func (s *StateDB) GetCodeSize(addr common.Address) int {
+	defer debug.Handler.StartRegionAuto("StateDB.GetCodeSize")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		if s.witness != nil {
@@ -471,6 +479,7 @@ func (s *StateDB) GetCodeSize(addr common.Address) int {
 }
 
 func (s *StateDB) GetCodeHash(addr common.Address) common.Hash {
+	defer debug.Handler.StartRegionAuto("StateDB.GetCodeHash")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return common.BytesToHash(stateObject.CodeHash())
@@ -480,7 +489,7 @@ func (s *StateDB) GetCodeHash(addr common.Address) common.Hash {
 
 // GetState retrieves the value associated with the specific key.
 func (s *StateDB) GetState(addr common.Address, hash common.Hash) common.Hash {
-	defer debug.Handler.StartRegionAuto("GetState")()
+	defer debug.Handler.StartRegionAuto("StateDB.GetState")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.GetState(hash)
@@ -491,6 +500,7 @@ func (s *StateDB) GetState(addr common.Address, hash common.Hash) common.Hash {
 // GetCommittedState retrieves the value associated with the specific key
 // without any mutations caused in the current execution.
 func (s *StateDB) GetCommittedState(addr common.Address, hash common.Hash) common.Hash {
+	defer debug.Handler.StartRegionAuto("StateDB.GetCommittedState")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.GetCommittedState(hash)
@@ -504,6 +514,7 @@ func (s *StateDB) Database() Database {
 }
 
 func (s *StateDB) HasSelfDestructed(addr common.Address) bool {
+	defer debug.Handler.StartRegionAuto("StateDB.HasSelfDestructed")()
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.selfDestructed
@@ -569,6 +580,7 @@ func (s *StateDB) SetState(addr common.Address, key, value common.Hash) common.H
 // storage. This function should only be used for debugging and the mutations
 // must be discarded afterwards.
 func (s *StateDB) SetStorage(addr common.Address, storage map[common.Hash]common.Hash) {
+	defer debug.Handler.StartRegionAuto("StateDB.SetStorage")()
 	// SetStorage needs to wipe the existing storage. We achieve this by marking
 	// the account as self-destructed in this block. The effect is that storage
 	// lookups will not hit the disk, as it is assumed that the disk data belongs
@@ -600,6 +612,7 @@ func (s *StateDB) SetStorage(addr common.Address, storage map[common.Hash]common
 // The account's state object is still available until the state is committed,
 // getStateObject will return a non-nil account after SelfDestruct.
 func (s *StateDB) SelfDestruct(addr common.Address) uint256.Int {
+	defer debug.Handler.StartRegionAuto("StateDB.SelfDestruct")()
 	stateObject := s.getStateObject(addr)
 	var prevBalance uint256.Int
 	if stateObject == nil {
@@ -621,6 +634,7 @@ func (s *StateDB) SelfDestruct(addr common.Address) uint256.Int {
 }
 
 func (s *StateDB) SelfDestruct6780(addr common.Address) (uint256.Int, bool) {
+	defer debug.Handler.StartRegionAuto("StateDB.SelfDestruct6780")()
 	stateObject := s.getStateObject(addr)
 	if stateObject == nil {
 		return uint256.Int{}, false
@@ -762,6 +776,7 @@ func (s *StateDB) CreateAccount(addr common.Address) {
 // This operation sets the 'newContract'-flag, which is required in order to
 // correctly handle EIP-6780 'delete-in-same-transaction' logic.
 func (s *StateDB) CreateContract(addr common.Address) {
+	defer debug.Handler.StartRegionAuto("StateDB.CreateContract")()
 	obj := s.getStateObject(addr)
 	if !obj.newContract {
 		obj.newContract = true
