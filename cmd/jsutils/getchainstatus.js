cacf65c4b75d8e9aa1ed6655f1b91f5c7b7b6a60
zzzckck <152148891+zzzckck@users.noreply.github.com>
2025-06-12 14:54:33 +0800

    update

--- cmd/jsutils/getchainstatus.js
+++ cmd/jsutils/getchainstatus.js
@@ -247,7 +247,7 @@ const builderMap = new Map([
     ["0xb49f86586a840AB9920D2f340a85586E50FD30a2", "inblock eu"],
     ["0x0F6D8b72F3687de6f2824903a83B3ba13c0e88A0", "inblock us"],
     //      nodereal
-    ["0x79102dB16781ddDfF63F301C9Be557Fd1Dd48fA0", "nodereal ap"],
+    ["0x79102dB16781ddDfF63F301C9Be557Fd1Dd48fA0", "nodereal us"],
     ["0xd0d56b330a0dea077208b96910ce452fd77e1b6f", "nodereal eu"],
     ["0x4f24ce4cd03a6503de97cf139af2c26347930b99", "nodereal us"],
     //      xzbuilder
@@ -259,7 +259,7 @@ const builderMap = new Map([
     ["0x95c8436143c82Ea4d3529A3ed8DDa9998F6daC5F", "blockrazor eu"],
     ["0xb71Ba9e570ee20E983De1d5aE01baf5dCB4e4299", "blockrazor us"],
     ["0xA8caEc0D68a90Ac971EA1aDEFA1747447e1f9871", "blockroute"],
-    ["0x5EC60f73f938e36400ec3CC3Ff4d7a7703F7c005", "fuji us"],
+    ["0x5EC60f73f938e36400ec3CC3Ff4d7a7703F7c005", "fuji ap"],
     ["0x6C98EB21139F6E12db5b78a4AeD4d8eBA147FB7b", "nodereal eu"],
     ["0x7b3ee856c98b1bb3689ef7f90477df2927fcbdb6",  "trustnet"],
     ["0x627fE6AFA2E84e461CB7AE7C2c46e8adf9a954a2", "txboost"],
